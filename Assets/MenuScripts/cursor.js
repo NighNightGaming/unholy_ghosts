f4b390231c35ee78f53e83094e61d09218563967
Michael Perez <michaelp193@gmail.com>
2014-05-02 00:06:42 -0400

    get ready for betaaaa

---
+++ Assets/MenuScripts/cursor.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+//custom cursor
+var cursorTexture : Texture2D;
+
+//use auto for better compatability
+var cursorMode : CursorMode = CursorMode.ForceSoftware;
+
+var hotSpot : Vector2 = Vector2.zero;
+
+function OnMouseEnter() {
+	Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+}
+
+function OnMouseExit() {
+	Cursor.SetCursor(null, Vector2.zero, cursorMode);
+}
